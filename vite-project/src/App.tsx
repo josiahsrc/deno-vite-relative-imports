1904d7e58197117cd578bb7509c6707c1f611737
Josiah Saunders <contact@josiahsaunders.com>
2024-11-07 17:25:11 -0700

    vite works!

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 import { add } from "shared";
 import React from "react";
@@ -11,12 +9,12 @@ function App() {
   return (
     <>
       <div>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
